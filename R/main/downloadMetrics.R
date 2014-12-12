e5937baf9c8c68473c46d9f535d28de644435e08
Chris Sipola <chrissip@zillow.com>
2014-12-11 19:11:13 -0800

    Change file DL method

--- R/main/downloadMetrics.R
+++ R/main/downloadMetrics.R
@@ -1,15 +1,13 @@
 downloadMetrics <- function(
     yrs,
     projDir,
-    email,
-    nFiles = NULL,  # nFiles argument from downloadGsod
     isDebug = TRUE
 )
-
+    
 {
     # ######################################################################### 
     # Populate priDir/rawData/ with NOAA Global Surface Summary of Day (GSOD) 
-    # weather metrics (loops over other function). 
+    # weather metrics. 
     # #########################################################################
     
     # Define paths, function name, etc.
@@ -21,19 +19,25 @@ downloadMetrics <- function(
     e <- environment()
     invisible(sapply(helpers, sys.source, envir = e))
     
-    fwfsList <- list()
     for (yr in yrs)
     {
-        fwfsList[[as.character(yr)]] <- downloadMetricsYear(
-            yr      = yr,
-            projDir = projDir,
-            email   = email,
-            nFiles  = nFiles,
-            isDebug = isDebug
-        )
+        # Download .tar file
+        if (isDebug) debugCat(fun, "downloading .tar for", paste0(yr, "..."))
+        gsodPath <- "ftp://ftp.ncdc.noaa.gov/pub/data/gsod"
+        tarFilename <- paste0("gsod_", yr, ".tar")
+        tarUrl <- file.path(gsodPath, yr, tarFilename)
+        tarPath <- file.path(projDir, "rawData", tarFilename)
+        download.file(url = tarUrl, destfile = tarPath, method = "auto", mode = "wb")
+        
+        # Untar into new folder and delete .tar file
+        if (isDebug) debugCat(fun, "extracting files for", paste0(yr, "..."))
+        yrDir <- file.path(projDir, "rawData", paste0("gsod_", yr))
+        dir.create(yrDir, showWarnings = FALSE)
+        untar(tarfile = tarPath, exdir = yrDir)
+        file.remove(tarPath)
     }
     
-    fwfs <- do.call(rbind, fwfsList)
+    if (isDebug) debugCat(fun, "done!")
     
-    return(invisible(fwfs))
+    return(invisible(TRUE))
 }
